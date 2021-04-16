cea57edf0086f32047c4ad3e6917387d39efdf68
Marissa Paramo <mp311428@myoc.odessa.edu>
2021-04-16 13:03:17 -0500

    Reviews Array

---
+++ src/shared/reviews.js
@@ -0,0 +1,73 @@
+export const REVIEWS = 
+[
+    {
+        id: 0,
+        rating: 5, 
+        text: "Had an issue with outlet under the kitchen sink, rendering my garbage disposal and sink useless for Thanksgiving. Called State Code Electric Friday morning and Antonio (owner) was able to fit me in. Did a great job at a reasonable price and kept me informed on everything he was doing the entire time. State Code Electric is the only electrician I will consider for future work in the Baytown area!",
+        author: "Tim Burns",
+        date: "2020-8-18T10:28Z"
+    },
+    {
+        id: 1,
+        rating: 5, 
+        text: "We needed to have a ceiling fan put in the living room of our two story home. There seemed to be no way possible to get power to the center of the ceiling but State Code Electric found a way. He didn't give up and found a solution to what seemed an impossible task. I highly recommend him and will definitely use him again for any electrical issues we run into. Thanks State Code Electric for your hard work.",
+        author: "Sheila Joy",
+        date: "2021-01-29T03:33Z"
+    },
+    {
+        id: 2,
+        rating: 5, 
+        text: "OMG this company deserves five stars all the way around. Antonio who I believe is the owner promptly call me back after I left a message and was able to come out and give me an estimate the following day. Due to it is summertime his schedule was pretty booked but he knew that I wanted my issue to be addressed as soon as possible and cleared his morning schedule for me just to fix my problem. I am so grateful and thankful Antonio and his team. He was very professional, prompt, polite and affordable. I will use this company from now on. Thank you!",
+        author: "Sara Williams",
+        date: "2020-10-30T18:12Z"
+    },
+    {
+        id: 3,
+        rating: 4, 
+        text: "The electrician was very nice and explained everything to me and I was able to make a great decision. They worked quickly to fix my problems. I would recommend them for any electrical problems.",
+        author: "Jaren Smith",
+        date: "2020-07-02T20:11Z"
+    },
+    {
+        id: 4,
+        rating: 5, 
+        text: "Antonio is the total package. He is very professional, patient, efficient, and reasonably priced. He is pleasant and easy to talk to. I am very happy with his service and I highly recommend him.",
+        author: "Carlos Hernandez",
+        date: "2021-03-14T15:51Z"
+    },
+    {
+        id: 5,
+        rating: 4, 
+        text: "Overall good service in my opinion. They resolve water heater issue also.",
+        author: "Julie Star",
+        date: "2020-12-01T08:33Z"
+    },
+    {
+        id: 6,
+        rating: 5, 
+        text: "This is the 2nd time we have called State Code Electric and we definitely recommend them. He has been super quick to reply both times we called and has shown up within a few hours each time!  He fixed both issues quickly (one was an electrical issue in the main living area, the other was a faulty garage door), he explains what he did to fix the issues, and he is super friendly!  Don't hesitate to call State Code Electric!",
+        author: "Brittany Bat",
+        date: "2021-02-18T18:12Z"
+    },
+    {
+        id: 7,
+        rating: 5, 
+        text: "I have used Stats Code Electric to maintain my units for the past few years. Antonio has always given me honest and fair service and recommendations and has gone above and beyond to work with my hectic schedule. He works fast and efficiently. I highly recommend him.",
+        author: "Mario More",
+        date: "2021-03-04T20:11Z"
+    },
+    {
+        id: 8,
+        rating: 4, 
+        text: "Antonio did an outstanding job. I had to wire up a barn with a light and two outlets way out in the country. Antonio drove a long way (outside of his normal service area) to make it happen. Antonio did a great job. I would highly recommend you call him.",
+        author: "Gabriel Sanchez",
+        date: "2021-04-08T19:44Z"
+    },
+    {
+        id: 9,
+        rating: 5, 
+        text: "We are very happy with the job that was done. Antonio is easy-going and very professional. He was able to fix the problem in less than an hour. He is affordable and I would recommended him to a friend.",
+        author: "Kelsey Summer",
+        date: "2020-05-25T16:30Z"
+    }
+]
