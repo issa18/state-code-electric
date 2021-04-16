a86f917f175bcae3e07523f1eaa1b6ed5f4b15a5
Marissa Paramo <mp311428@myoc.odessa.edu>
2021-04-16 15:20:23 -0500

    Added React-Router-Dom Package. Add a Main component linked to App.js

---
+++ src/components/MainComponent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            gallery: GAllERY
+        };
+    }
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Main;
