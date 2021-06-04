5574bc339631737e7fa1926e02768c5da385113c
Marissa Paramo <mp311428@myoc.odessa.edu>
2021-06-04 11:45:17 -0500

    Merged AboutUs branch

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
+import 'Fredericka the Great';
 import App from './App';
 
 ReactDOM.render(
